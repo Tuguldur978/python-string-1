741fc571e8228d33a5cbcbd65d7ccb37444678b8
Tuguldur978 <tuguldur.s2016@orkhonschool.edu.mn>
2026-09-21 09:35:58 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,12 +2,10 @@
 # Exercise 1
 def count_characters(text):
     return len(text)
-print(count_characters("Hello World"))
     
 # Exercise 2
 def remove_spaces(text):
     return text.replace(" ", "")
-print(remove_spaces("Python is fun")
 
 # Exercise 3
 def count_vowels(text):
@@ -17,4 +15,3 @@ def count_vowels(text):
         if letter in vowels :
             count += 1
     return count
-print(count_vowels("Python is amazing"))
