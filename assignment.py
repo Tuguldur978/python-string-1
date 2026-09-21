73032d655f4f297003c3d7c1b83135a88a047258
Tuguldur978 <tuguldur.s2016@orkhonschool.edu.mn>
2026-09-21 08:51:13 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,9 +2,10 @@
 
 # Exercise 1
 def count_characters(text):
-    # Write your code here
-    pass
-
+    return len(text)
+text = input()
+print(count_characters(text))
+    
 # Exercise 2
 def remove_spaces(text):
     # Write your code here
